90c7f41ef13ef7b4f00b94a4ca0f8dcbd15960de
Mari Husain <maryamhusain98@gmail.com>
2017-06-02 16:27:10 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   # static_pages_controller actions
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-
-  # application_controller actions
-  root 'application#hello'
 end
